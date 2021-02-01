a89afdcb31463a8f541efb96b509ad16ce4b11f8
liweiping <18819446836@163.com>
2021-02-01 17:35:00 +0800

    修改不正确语法

---
+++ app/src/main/java/com/shui520it/access/funcion/action/ShuiBaseAction.kt
@@ -0,0 +1,7 @@
+package com.shui520it.access.funcion.action
+
+/**
+ * @author shuimu{lwp}
+ * @time  2021/2/1 13:44
+ * @desc
+ */
---
+++ app/src/main/java/com/shui520it/access/funcion/action/ShuiIdActionImpl.kt
@@ -0,0 +1,20 @@
+package com.shui520it.access.funcion.action
+
+import android.os.Build
+import android.view.accessibility.AccessibilityNodeInfo
+import androidx.annotation.RequiresApi
+
+/**
+ * @author shuimu{lwp}
+ * @time  2021/2/1 13:58
+ * @desc 根据具体的控件ID寻找 该控件对应的 AccessibilityNodeInfo映射体
+ */
+class ShuiIdActionImpl : ShuiBaseAction<String>() {
+
+    override fun findFirst(info: AccessibilityNodeInfo): AccessibilityNodeInfo? =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) info.findAccessibilityNodeInfosByViewId(acitonKey)?.takeIf { it.isNotEmpty() }?.first() else null
+
+    override fun findAll(info: AccessibilityNodeInfo): List<AccessibilityNodeInfo>? =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) info.findAccessibilityNodeInfosByViewId(acitonKey) else null
+
+}
